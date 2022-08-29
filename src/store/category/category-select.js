86013b16cf57dc80785b536c1479c8b090e3ac4f
Bashsoft707 <alausabashir@gmail.com>
2022-08-29 18:46:24 +0100

    acc dreateSelector (memoization of selector function) to category

--- src/store/category/category-select.js
+++ src/store/category/category-select.js
@@ -1,1 +1,8 @@
-export const selectCategory = (state) => state.category.categoryMap;
+import { createSelector } from "reselect";
+
+const selectCategoryMap = (state) => state.category;
+
+export const selectCategory = createSelector(
+  [selectCategoryMap],
+  (category) => category.categoryMap
+);
