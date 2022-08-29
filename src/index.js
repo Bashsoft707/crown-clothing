37f36f10870b0af87f427964d0c4f6b1c04b4a3c
Bashsoft707 <alausabashir@gmail.com>
2022-08-29 19:05:28 +0100

    adding redux-persist to our application

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,19 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { createLogger } from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const middleware = createLogger();
 const composeEnhancer = compose(applyMiddleware(middleware));
 
-export const store = createStore(rootReducer, undefined, composeEnhancer);
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistEnhancer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistEnhancer, undefined, composeEnhancer);
+
+export const persistor = persistStore(store);
