57660ca18aab41e9520a99a07be995d0c517494b
Bashsoft707 <alausabashir@gmail.com>
2023-06-25 10:39:54 -0700

    add payment to checkout

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../../context/cartContext";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import { PaymentForm } from "../../components/payment-form/payment-form.component";
+import { Navigate } from "react-router-dom";
 
 import {
   CheckoutContainer,
@@ -8,11 +10,14 @@ import {
   HeaderBlock,
   Total,
 } from "./checkout.styles";
+import { memo } from "react";
+import { UserContext } from "../../context/userContext";
 
-export const Checkout = () => {
+export const Checkout = memo(() => {
   const { cartItems, cartItemTotal } = useContext(CartContext);
+  const { currentUser } = useContext(UserContext);
 
-  return (
+  return currentUser ? (
     <CheckoutContainer>
       <CheckoutHeader>
         <HeaderBlock>
@@ -35,6 +40,9 @@ export const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartItemTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
+  ) : (
+    <Navigate to={{ pathname: "/auth" }} />
   );
-};
+});
